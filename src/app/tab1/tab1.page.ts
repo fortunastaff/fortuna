864537d025333f5d4e27516d8b5132b2e2c8608d
Hector <hecmartp@gmail.com>
2019-04-23 15:35:58 +0200

    Update tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  
+    ranking: any [] = [];
+
+  constructor ( private http: HttpClient ){
+  console.log ('constructor log ');
+  this.http.get('https://pinkbean.xyz/api/rank/eu/xpanquesin/overall/')
+    .subscribe( (resp: any) => {
+      this.ranking = resp;
+      console.log (resp);
+     })
+  
+}
+}
